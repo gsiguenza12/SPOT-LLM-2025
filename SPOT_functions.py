8a58946793daa83e0d1793e05161da70660057de
Gabriel Alfredo Siguenza <gebsiguenza@gmail.com>
2024-11-21 17:09:02 -0800

    Add files via upload

---
+++ SPOT_functions.py
@@ -0,0 +1,142 @@
+import bosdyn.client
+import bosdyn.client.util
+from bosdyn.client.robot_command import RobotCommandClient, RobotCommandBuilder, blocking_stand, blocking_sit
+import os, time
+
+def authenticate_SPOT(robot):
+    """
+    function to authenticate to a SPOT ROBOT.
+    If authentication is successfull: 
+        the function will attempt to turn off estop and take control of the lease
+    An exception will be raised and the reason will be given to the console.
+    @return: True if success, False otherwise
+    """
+    try:
+        bosdyn.client.util.authenticate(robot) #authenticate using bosdyn generic function
+        robot.time_sync.wait_for_sync()
+        
+        estop_client = robot.ensure_client('estop')
+        estop_endpoint = bosdyn.client.estop.EstopEndpoint(client=estop_client, name='spotgpt_estop', estop_timeout=25.0)   #power cutoff after 25 seconds
+        estop_endpoint.force_simple_setup()
+        estop_keep_alive = bosdyn.client.estop.EstopKeepAlive(estop_endpoint)
+        print(estop_client.get_status())
+
+        lease_client = robot.ensure_client('lease')
+        lease = lease_client.acquire()
+        lease_keep_alive = bosdyn.client.lease.LeaseKeepAlive(lease_client, return_at_exit="False")
+        return True
+    except Exception as e:
+        print("Authentication initialization failed.\nReason: " + str(e))
+        return False 
+
+def power_on_spot(robot):
+    """
+    function to power on spot
+    """
+    try:
+        if robot.is_powered_on():
+            print("Robot is already on")
+        else:
+            robot.power_on(timeout_sec=20)
+    except Exception as e:
+        print("Failed to powering on SPOT.\nReason:" + str(e))
+
+def power_off_spot(robot):
+    """
+    function to power off spot
+    """
+    try:
+        if not robot.is_powered_on():
+            print("Robot is already off")
+        else:
+            command_client = robot.ensure_client(RobotCommandClient.default_service_name)
+            stop_command = RobotCommandBuilder.stop_command()
+            selfright_command = RobotCommandBuilder.selfright_command()
+            safe_power_off_command = RobotCommandBuilder.safe_power_off_command()
+            command_client.robot_command(stop_command)
+            command_client.robot_command(selfright_command)
+            command_client.robot_command(safe_power_off_command)
+    except Exception as e:
+        print("Failed to powering off SPOT.\nReason:" + str(e))
+
+def move_spot_forward(robot, distance_m, distance_unit, event):
+    UNITS = {"seconds", "minutes"}
+    try:
+        distance_m = int(distance_m)
+        if distance_unit not in UNITS:
+            raise Exception("Invalid distance time unit")
+        if distance_unit == "minutes":
+            distance_m = distance_m * 60
+        if distance_m > 30:
+            raise Exception("Exceed distance limit")
+        if not robot.is_powered_on():
+            raise Exception("SPOT is off")
+        command_client = robot.ensure_client(RobotCommandClient.default_service_name)
+        blocking_stand(command_client, timeout_sec=10)
+
+        # Build a velocity command in the robot's frame. Positive x is forward.
+        vel_cmd = RobotCommandBuilder.synchro_velocity_command(v_x=0.5, v_y=0, 
+                                                    v_rot=0)
+        
+        # Calculate the time needed to travel the desired distance at the given speed.
+        travel_time_s = distance_m / 0.5
+        
+        # Issue the command to start moving
+        command_client.robot_command(lease=None, command=vel_cmd,
+                                    end_time_secs=time.time() + travel_time_s)
+        
+        # Wait for Spot to finish moving
+        event.wait(travel_time_s)
+        
+        # After moving the desired distance, stop Spot by sending a zero velocity command
+        stop_cmd = RobotCommandBuilder.stop_command()
+        command_client.robot_command(lease=None, command=stop_cmd)
+        # blocking_sit(command_client, timeout_sec=10)
+    except Exception as e:
+        print("Failed to move spot SPOT.\nReason:" + str(e))
+
+# TEST
+# def move_spot_backward(robot, distance_m, distance_unit, event):
+#     UNITS = {"seconds", "minutes"}
+#     try:
+#         distance_m = int(distance_m)
+#         if distance_unit not in UNITS:
+#             raise Exception("Invalid distance time unit")
+#         if distance_unit == "minutes":
+#             distance_m = distance_m * 60
+#         if distance_m > 30:
+#             raise Exception("Exceed distance limit")
+#         if not robot.is_powered_on():
+#             raise Exception("SPOT is off")
+#         command_client = robot.ensure_client(RobotCommandClient.default_service_name)
+#         blocking_stand(command_client, timeout_sec=10)
+# 
+#         # Build a velocity command in the robot's frame. Positive x is forward. change to move in opposite direction
+#         vel_cmd = RobotCommandBuilder.synchro_velocity_command(v_x=0.5, v_y=0,
+#                                                                v_rot=0)
+# 
+#         # Calculate the time needed to travel the desired distance at the given speed.
+#         travel_time_s = distance_m / 0.5
+# 
+#         # Issue the command to start moving
+#         command_client.robot_command(lease=None, command=vel_cmd,
+#                                      end_time_secs=time.time() + travel_time_s)
+# 
+#         # Wait for Spot to finish moving
+#         event.wait(travel_time_s)
+# 
+#         # After moving the desired distance, stop Spot by sending a zero velocity command
+#         stop_cmd = RobotCommandBuilder.stop_command()
+#         command_client.robot_command(lease=None, command=stop_cmd)
+#         blocking_sit(command_client, timeout_sec=10)
+#     except Exception as e:
+#         print("Failed to move spot SPOT.\nReason:" + str(e))
+
+def stop_spot(robot):
+    try:
+        print("Stopping spot from the current action")
+        command_client = robot.ensure_client(RobotCommandClient.default_service_name)
+        stop_cmd = RobotCommandBuilder.stop_command()
+        command_client.robot_command(lease=None, command=stop_cmd)
+    except Exception as e:
+        print("Failed to stop spot. Reason: " + str(e))
---
+++ gpt_module.py
@@ -0,0 +1,128 @@
+from openai import OpenAI
+import os
+
+"""
+The provided code defines a list of tools, each represented as a dictionary. 
+These tools are functions designed to control a SPOT robot, a type of robotic system. 
+Each dictionary within the list specifies the type of tool (in this case, all are functions) 
+and details about the function itself, including its name, description, and any parameters it requires. 
+This provides a structured way to define and document the functions available for controlling a SPOT robot, 
+ensuring that each function is clearly described and that any required parameters are well-defined.
+"""
+
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key="sk-redacted",
+)
+# set the output location
+current_directory = os.getcwd()
+# file_path = os.path.join(current_directory, 'src', 'gpt_output', 'output.json')
+file_path = os.path.join(current_directory, 'gpt_output', 'output.json')
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "move_spot_forward",
+            "description": "Command a SPOT robot to move forward",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "time": {
+                        "type": "integer",
+                        "description": "The time it takes for the robot to move forward.",
+                    },
+                    "time_format": {
+                        "type": "string",
+                        "enum": ["seconds", "minutes"],
+                        "description": "The unit of time for this command. Make sure that it is either seconds or minutes. "
+                        "Do not accept any other time format as a valid format. Ensure the time does not exceed 30 seconds",
+                    },
+                },
+                "required": ["time", "time_format"],
+            },
+        }
+    },
+    # ADD WALK BACKWARD FUNCTION
+    {
+        "type": "function",
+        "function": {
+            "name": "power_on",
+            "description": "Command a SPOT robot to power on",
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "power_off",
+            "description": "Command a SPOT robot to power off",
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "stop",
+            "description": "Command SPOT robot to stop the current action",
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "quit",
+            "description": "Stop SPOT from the current action and quit the program"
+        }
+    }
+]
+
+def generate_function(user_input):
+    """
+    Generates a response using the GPT-3.5-turbo model based on the provided user input.
+    Args:
+        user_input (str): The input prompt from the user.
+    Returns:
+        completion: The completion object containing the generated response.
+    The function performs the following steps:
+    1. Initializes a list of messages with a system message and the user input.
+    2. Calls the GPT-3.5-turbo model to generate a response.
+    3. Continuously updates the conversation with the assistant's messages and user inputs until the response is complete.
+    4. Writes the final response to an output file in JSON format.
+    5. Ensures the directory for the output file exists, creating it if necessary.
+    """
+    '''
+    Call gpt using the current settings
+    if user_input prompt is valid:
+      gpt will create a response of the current function_name with arguments parameter based of actions
+      completion.choices[0] need to be == tool_calls to generate this valid response
+    '''
+    messages = []
+    messages.append({"role": "system", "content": "Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous."})
+    messages.append({"role": "user", "content": user_input})
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        tools=tools
+    )
+    while completion.choices[0].finish_reason == "stop":
+        assistant_message = completion.choices[0].message
+        messages.append(assistant_message)
+        print(assistant_message.content)
+        user_input = input("")
+        messages.append({"role": "user", "content": user_input})
+        
+        completion = client.chat.completions.create(
+          model="gpt-3.5-turbo",
+          messages=messages,
+          tools=tools
+        )
+        
+    # with open(file_path, "w") as outfile:  # Write the response to output.json
+    #     outfile.write(completion.model_dump_json())
+    # return completion
+    # Ensure the directory exists
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    with open(file_path, "w") as outfile:  # Write the response to output.json
+        outfile.write(completion.model_dump_json())
+    return completion
